3c5e72a51872bcfd97984a1b50a37a85b0306580
alexz <derk153@gmail.com>
2015-11-04 13:14:48 +0200

    install yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name derk153githubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the derk153githubioApp
+ */
+angular.module('derk153githubioApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name derk153githubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the derk153githubioApp
+ */
+angular.module('derk153githubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
